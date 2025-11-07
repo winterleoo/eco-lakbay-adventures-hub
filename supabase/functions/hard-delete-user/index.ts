82de032f49698886ec993b16f917b15e2f305baa
winterleoo <johnleomedina@gmail.com>
2025-11-07 23:29:00 +0800

    update

--- supabase/functions/hard-delete-user/index.ts
+++ supabase/functions/hard-delete-user/index.ts
@@ -1,5 +1,5 @@
 import { serve } from 'https://deno.land/std@0.168.0/http/server.ts'
-import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2/dist/module/index.js";
 
 serve(async (req) => {
   // CORS logic for preflight request
